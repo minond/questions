0a6fadbdbde7bf7737c10a18ef01d91e02f5ef1d
Marcos Minond <minond.marcos@gmail.com>
2018-04-13 17:00:09 -0600

    single choice question test

---
+++ src/test/java/urect/questions/SingleChoiceQuestionTest.java
@@ -0,0 +1,62 @@
+package urect.questions;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class SingleChoiceQuestionTest {
+  @Test
+  public void testSingleChoiceQuestionWithNoTextIsInvalid() {
+    Question q = new SingleChoiceQuestion("", new ArrayList<Answer>());
+    assertFalse(q.isValid());
+  }
+
+  @Test
+  public void testSingleChoiceQuestionWithNoAnswersIsInvalid() {
+    Question q = new SingleChoiceQuestion("q1", new ArrayList<Answer>());
+    assertFalse(q.isValid());
+  }
+
+  @Test
+  public void testSingleChoiceQuestionWithNoCorrectAnswersIsInvalid() {
+    List<Answer> answers =
+        Arrays.asList(
+            new Answer("1", false),
+            new Answer("2", false),
+            new Answer("3", false),
+            new Answer("4", false));
+
+    Question q = new SingleChoiceQuestion("q1", answers);
+    assertFalse(q.isValid());
+  }
+
+  @Test
+  public void testSingleChoiceQuestionWithMultipleCorrectAnswersIsInvalid() {
+    List<Answer> answers =
+        Arrays.asList(
+            new Answer("1", false),
+            new Answer("2", true),
+            new Answer("3", false),
+            new Answer("4", true));
+
+    Question q = new SingleChoiceQuestion("q1", answers);
+    assertFalse(q.isValid());
+  }
+
+  @Test
+  public void testSingleChoiceQuestionWithTextAndOneCorrectAnswerIsValid() {
+    List<Answer> answers =
+        Arrays.asList(
+            new Answer("1", false),
+            new Answer("2", true),
+            new Answer("3", false),
+            new Answer("4", false));
+
+    Question q = new SingleChoiceQuestion("q1", answers);
+    assertTrue(q.isValid());
+  }
+}
